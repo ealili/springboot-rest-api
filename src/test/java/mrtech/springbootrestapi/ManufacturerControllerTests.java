39ae2d17d318046a469c53145bb17f108ba002b4
Lenart Ibraimi <leni_carbon9@hotmail.com>
2020-02-04 22:17:39 +0100

    Add unit testing for GET method in ManufacturerController

---
+++ src/test/java/mrtech/springbootrestapi/ManufacturerControllerTests.java
@@ -0,0 +1,43 @@
+package mrtech.springbootrestapi;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import mrtech.springbootrestapi.controller.ManufacturerController;
+import mrtech.springbootrestapi.pojo.Manufacturer;
+import mrtech.springbootrestapi.service.manufacturer.ManufacturerService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ManufacturerController.class)
+public class ManufacturerControllerTests {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private ManufacturerService manufacturerService;
+
+    @Test
+    public void getManufacturersTest() throws Exception {
+        mvc.perform(get("/api/manufacturers")).andExpect(status().isOk());
+    }
+
+
+
+    private String toJson(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
