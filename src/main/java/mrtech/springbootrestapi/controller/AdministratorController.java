125a7e3fea5994d73fc6a79e7376c3e860957034
Lenart Ibraimi <leni_carbon9@hotmail.com>
2020-02-04 21:46:53 +0100

    Add unit testing for POST method in AdministratorController

--- src/main/java/mrtech/springbootrestapi/controller/AdministratorController.java
+++ src/main/java/mrtech/springbootrestapi/controller/AdministratorController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api", method = RequestMethod.PUT)
 public class AdministratorController {
 
     @Autowired
